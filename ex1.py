80d3d4c763bc40b3b430e60374fda3b1d4a71ed1
smtm1209 <sarkistm@hotmail.com>
2018-03-24 18:53:05 -0400

    Add Example Files

---
+++ ex1.py
@@ -0,0 +1,25 @@
+import pybeam
+
+Y = pybeam.get_source_matrix()
+print(Y, flush=True)
+
+X = pybeam.get_verification_matrix(b=(45,90))
+print(X, flush=True)
+
+sig, samp_freq, dtype = pybeam.read_wav_file('btest.wav')
+print(sig, samp_freq, dtype, sep='\n', flush=True)
+
+Q = pybeam.get_DAS_filters(X=X, Y=Y)
+print(Q, flush=True)
+
+output = pybeam.map_filters(Q, sig)
+print(output, flush=True)
+
+mapping = {0:4}
+pybeam.write_wav_dir('ex1', output, mapping, samp_freq)
+
+pybeam.playback_wav_dir('ex1')
+
+
+
+
